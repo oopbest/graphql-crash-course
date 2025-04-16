1d464e29c007e44a99831711b504442a202867ba
Setthawut Pimthong <oop_best@yahoo.com>
2025-04-16 17:00:10 +0700

    authentication handled on the client

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import TransactionPage from "./pages/TransactionPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+
+import Header from "./components/ui/Header.jsx";
+import { useQuery } from "@apollo/client";
+import { Toaster } from "react-hot-toast";
+import { GET_AUTHENTICATED_USER } from "./graphql/queries/user.query.js";
+
+function App() {
+  const { data, loading, error } = useQuery(GET_AUTHENTICATED_USER);
+
+  if (loading) return null;
+  return (
+    <>
+      {data.authUser && <Header />}
+      <Routes>
+        <Route
+          path="/"
+          element={data.authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!data.authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!data.authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/transaction"
+          element={
+            data.authUser ? <TransactionPage /> : <Navigate to="/login" />
+          }
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Toaster />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { GridBackground } from "./components/ui/GridBackground.jsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(), // Apollo Client uses to cache queries results after fetching them
+  credentials: "include", // This tells Apollo Cilent to send cookies to the backend
+});
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <GridBackground>
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
+      </GridBackground>
+    </BrowserRouter>
+  </StrictMode>
+);
